1c004d0972b762b28e2fb64bb3e7fa0ece717b13
Bhomale Prajwal <bhomaleprajwal19@gmail.com>
2025-02-01 10:24:25 +0530

    fixed 3

--- db/connectDb.js
+++ db/connectDb.js
@@ -4,7 +4,9 @@ const connectDb = async () => {
     try {
         // Use process.env.MONGODB_URI for flexibility and security
         const conn = await mongoose.connect("mongodb+srv://ray:5BFfop3AXZqwNlw6@passkeep.fkpov.mongodb.net/thankbank", {
-           
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
         return conn;
